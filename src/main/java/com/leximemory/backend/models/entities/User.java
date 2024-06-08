1fde92ece3151eff575da9bc3c4530865a186031
Vinicius Silva Virgilli <vinicius.virgilli3@gmail.com>
2024-06-08 19:37:43 -0300

    (/models/entities): create User entity

---
+++ src/main/java/com/leximemory/backend/models/entities/User.java
@@ -0,0 +1,70 @@
+package com.leximemory.backend.models.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private String name;
+  private String email;
+  private String password;
+  private String registrationDate;
+
+  public User() {
+  }
+
+  public User(Integer id, String name, String email, String password, String registrationDate) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.registrationDate = registrationDate;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getRegistrationDate() {
+    return registrationDate;
+  }
+
+  public void setRegistrationDate(String registrationDate) {
+    this.registrationDate = registrationDate;
+  }
+}
